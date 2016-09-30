55c347bc2bbc68856749bf76f9c71b243df84881
macsousa <macsousa@gmail.com>
2016-09-29 23:15:20 -0400

    Equacao de 2 grau
    Resolucao de equacoes de 2 grau

---
+++ equacoes2grau.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+# include <math.h> // Biblioteca com as funções matemáticas
+
+main() {
+	
+	int a,b,c;
+	float delta, x1, x2;
+	
+	printf("Resolucao de equacoes do 2 grau.\n");
+	printf("Digite valor de a: ");
+	scanf("%d", &a);
+
+	printf("Digite valor de b: ");
+	scanf("%d", &b);
+
+	printf("Digite valor de c: ");
+	scanf("%d", &c);
+	
+	delta = ( b*b ) - ( 4*a*c ); 
+	
+	if(delta > 0 ) {
+	   x1 = (-b + sqrt(delta)) / (2*a);
+	   x2 = (-b - sqrt(delta)) / (2*a);
+	} else if (delta == 0) {
+	   x1 = -b/(2*a);
+	   x2=x1;	
+	} else {  //delta < 0
+		
+	}
+	
+	
+	printf("x1 = %f\nx2 = %f", x1,x2);
+	
+}
